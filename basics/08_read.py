35c05dd0792c764d5a0c1205d2c110268270fb9d
aicdev <doc.aicdev@gmail.com>
2021-03-10 09:02:32 +0100

    update file example

--- basics/08_read.py
+++ basics/08_read.py
@@ -1,3 +1,7 @@
+import os
+import random
+import string
+
 # read data from a file
 
 # with open('./data/info.txt', 'r') as out:
@@ -7,13 +11,30 @@
 #         print(line.strip())
 #         line = out.readline()
 
-# read chunks in byte mode
 
-# chunck_size_in_bytes = 11
 
+# create huge demo file
+# with open('./data/huge.txt', 'w+') as huge_file:
+#     huge_file.write("header \n")
+
+#     current_size = os.stat('./data/huge.txt').st_size
+#     while current_size < 18253611008:
+#         huge_file.write('foobar lol \n' * 10000)
+#         current_size = os.stat('./data/huge.txt').st_size
+#         print("current size: ", current_size)
+
+# print("huge file generated")
+
+
+
+# 17 GB ~ 18253611008 bytes
+#chunck_size_in_bytes = 11
+
+#read chunks in byte mode
 # with open('./data/info.txt', 'rb') as out:
 #     chunk = out.read(chunck_size_in_bytes)
 
 #     while chunk:
-#         print(bytearray(chunk))
+#         byte_list = bytearray(chunk)
+#         print(byte_list)
 #         chunk = out.read(chunck_size_in_bytes)
